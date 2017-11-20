e7418f229af305ed7e2de6c4d73cbcf8b26d142e
loe <lo-e@outlook.com>
2017-11-21 00:04:31 +0800

    nothing

--- quant/test.py
+++ quant/test.py
@@ -4,6 +4,7 @@ import pymongo
 from vnpy.trader.app.ctaStrategy.ctaBase import TICK_DB_NAME
 from datetime import datetime, timedelta
 
+'''
 client = pymongo.MongoClient('localhost', 27017)
 collection = client[TICK_DB_NAME]['600380.SSE']
 flt = {'datetime':{'$gte':datetime.strptime('2017-6-6', '%Y-%m-%d'),
@@ -11,3 +12,5 @@ flt = {'datetime':{'$gte':datetime.strptime('2017-6-6', '%Y-%m-%d'),
 cursor = collection.find(flt).sort('datetime')
 for d in cursor:
     print d['datetime']
+    '''
+print 'yes'
