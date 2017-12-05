43e8cfe4fb915690da3a37fa0f2816476a12e973
loe <lo-e@outlook.com>
2017-12-05 20:26:25 +0800

    update

--- quant/test.py
+++ quant/test.py
@@ -1,16 +1,7 @@
 # -- coding: utf-8 --
 
-import pymongo
-from vnpy.trader.app.ctaStrategy.ctaBase import TICK_DB_NAME
-from datetime import datetime, timedelta
+import pandas as pd
+import tushare as ts
 
-'''
-client = pymongo.MongoClient('localhost', 27017)
-collection = client[TICK_DB_NAME]['600380.SSE']
-flt = {'datetime':{'$gte':datetime.strptime('2017-6-6', '%Y-%m-%d'),
-                    '$lt':datetime.strptime('2017-6-7', '%Y-%m-%d')}}
-cursor = collection.find(flt).sort('datetime')
-for d in cursor:
-    print d['datetime']
-    '''
-print 'yes'
+
+print 'abc'
