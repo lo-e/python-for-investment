e2238ad5ef5bb4b52fd05e965e576edfb5cf0bf9
loe <lo-e@outlook.com>
2018-01-17 17:01:41 +0800

    update

--- quant/test.py
+++ quant/test.py
@@ -3,6 +3,12 @@
 import pandas as pd
 import tushare as ts
 
+def nothing():
+    print 'no'
+
+def action():
+    print 'yes'
+    nothing()
+
 if __name__ == '__main__':
-    print 'abc'
-    print 'test'
+    action()
