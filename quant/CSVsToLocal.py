2ffdfe8dece9a28ccd654db286706a3c4a641021
loe <lo-e@outlook.com>
2018-02-11 15:16:31 +0800

    update

--- quant/CSVsToLocal.py
+++ quant/CSVsToLocal.py
@@ -17,11 +17,11 @@ MORNING_RESTART_CF = datetime.time(10, 30)
 MORNING_END_CF = datetime.time(11, 30)
 AFTERNOON_START_CF = datetime.time(13, 30)
 AFTERNOON_END_CF = datetime.time(15, 0)
-NIGHT_START_CF = datetime.time(21, 0)
 
-# 商品期货夜盘截至时间
-NIGHT_END_CF_1 = datetime.time(23, 0)
-NIGHT_END_CF_2 = datetime.time(23, 30)
+# 商品期货夜盘时间
+NIGHT_START_CF = datetime.time(21, 0)
+NIGHT_END_CF_N = datetime.time(23, 0) # 到夜间收盘
+NIGHT_END_CF_M = datetime.time(1, 0) # 到凌晨收盘
 
 #股指期货
 MORNING_START_SF = datetime.time(9, 30)
@@ -32,9 +32,9 @@ AFTERNOON_END_SF = datetime.time(15, 0)
 class csvsLocalEngine(object):
     def __init__(self, type, night):
         super(csvsLocalEngine, self).__init__()
-        # 数据类型，不同品种期货交易时间特别是夜盘时间会有所差别，为了剔除无效数据而设置 1、商品期货  2、股指期货
+        # 合约类型，不同品种期货交易时间特别是夜盘时间会有所差别，为了剔除无效数据而设置 1、商品期货  2、股指期货
         self.dataType = type
-        # 夜盘时间【1、（21:00 - 23:00) 2、(21:00 - 23:30)】
+        # 夜盘时间类型
         self.night = night
         # 项目路径
         self.walkingDir = 'CSVs'
@@ -55,6 +55,10 @@ class csvsLocalEngine(object):
                 if '\\' in root:
                     currentSymbol =  root.split('\\')[-1]
                     currentSymbol = currentSymbol  + '.TB'
+
+                # 排除不合法文件
+                if theFile.startswith('.'):
+                    continue
 
                 # 确定日期
                 if '.'in theFile:
@@ -86,28 +90,77 @@ class csvsLocalEngine(object):
                                 print '=' * 6, currentSymbol, ' ', currentDate.date(), '=' * 6
 
                             # 过滤无效数据
-                            tickTime = datetime.datetime.strptime(row['time'], '%H%M%S%f')
+                            timeStr = row['time']
+                            theLen = len(timeStr)
+                            if theLen < 9:
+                                sub = 9 - theLen
+                                timeStr = '0' * sub + timeStr
+
+                            '''
+                            hour = int(timeStr[0:2])
+                            minute = int(timeStr[2:4])
+                            second = int(timeStr[4:6])
+                            microSecond = int(timeStr[6:9])
+                            if hour < 0 or hour > 59 or minute < 0 or minute > 59 or second < 0 or second > 59 or microSecond < 0 or microSecond > 999:
+                                print u'删除时间格式错误数据：%s' % timeStr
+                                continue
+                            '''
+
+                            try:
+                                tickTime = datetime.datetime.strptime(timeStr, '%H%M%S%f')
+                            except Exception:
+                                print u'时间转换异常：%s' % timeStr
+                                raise
+                                exit(0)
+
                             t = tickTime.time()
                             fakeData = True
                             if self.dataType == 1:
                                 # 商品期货
-                                nightEndTime = NIGHT_END_CF_1
-                                if self.night == 2:
-                                    nightEndTime = NIGHT_END_CF_2
-                                if ((MORNING_START_CF <= t < MORNING_REST_CF) or (
-                                                MORNING_RESTART_CF <= t < MORNING_END_CF) or (
-                                                AFTERNOON_START_CF <= t < AFTERNOON_END_CF) or (
-                                                NIGHT_START_CF <= t < nightEndTime)):
-                                    fakeData = False
+                                if  self.night == 0:
+                                    # 无夜盘交易
+                                    if ((MORNING_START_CF <= t < MORNING_REST_CF)
+                                        or (MORNING_RESTART_CF <= t < MORNING_END_CF)
+                                        or (AFTERNOON_START_CF <= t < AFTERNOON_END_CF)):
+                                        fakeData = False
+                                elif self.night  ==  1:
+                                    # 夜间收盘
+                                    if ((MORNING_START_CF <= t < MORNING_REST_CF)
+                                        or (MORNING_RESTART_CF <= t < MORNING_END_CF)
+                                        or (AFTERNOON_START_CF <= t < AFTERNOON_END_CF)
+                                        or (NIGHT_START_CF <= t < NIGHT_END_CF_N)):
+                                        fakeData = False
+                                elif self.night == 2:
+                                    # 凌晨收盘
+                                    if ((MORNING_START_CF <= t < MORNING_REST_CF)
+                                        or (MORNING_RESTART_CF <= t < MORNING_END_CF)
+                                        or (AFTERNOON_START_CF <= t < AFTERNOON_END_CF)
+                                        or (NIGHT_START_CF <= t)
+                                        or (NIGHT_END_CF_M > t)):
+                                        fakeData = False
+
                             elif self.dataType == 2:
                                 # 股指期货
                                 if ((MORNING_START_SF <= t < MORNING_END_SF) or (
                                                 AFTERNOON_START_SF <= t < AFTERNOON_END_SF)):
                                     fakeData = False
 
+                            theDate = row['calendarday']
+                            theTime = tickTime.strftime('%H:%M:%S.%f')
+                            theDatetime = datetime.datetime.strptime(theDate + ' ' + theTime, '%Y%m%d %H:%M:%S.%f')
+
+                            # 数据库collection
+                            collection = self.db[currentSymbol]
+                            collection.create_index('datetime')
                             if fakeData:
                                 print t
                                 print u'【剔除无效数据】'
+                                # 删除已保存的数据库数据
+                                flt = {'datetime': theDatetime}
+                                cursor = collection.find(flt)
+                                if cursor.count():
+                                    print u'【剔除无效‘数据库’数据】'
+                                collection.delete_many(flt)
                                 continue
 
                             # 创建VtTickData对象
@@ -120,15 +173,11 @@ class csvsLocalEngine(object):
                             tick.askVolume1 = int(row['asksz1'])
                             tick.bidPrice1 = float(row['bid1'])
                             tick.bidVolume1 = int(row['bidsz1'])
-                            tick.date = row['calendarday']
-                            tick.time = tickTime.strftime('%H:%M:%S.%f')
-                            tick.datetime = datetime.datetime.strptime(tick.date + ' ' + tick.time, '%Y%m%d %H:%M:%S.%f')
-
-                            # 数据库collection
-                            collection = self.db[currentSymbol]
-                            collection.create_index('datetime')
+                            tick.date = theDate
+                            tick.time = theTime
+                            tick.datetime = theDatetime
                             # 保存tick到数据库
-                            collection.update_one({'datetime': tick.datetime}, {'$set': tick.__dict__}, upsert = True)
+                            collection.update_many({'datetime': tick.datetime}, {'$set': tick.__dict__}, upsert = True)
                         # 打印进程
                         if count:
                             sub = time() - startTime
@@ -181,9 +230,15 @@ class csvsLocalEngine(object):
 
 
 if __name__ == '__main__':
-    type = int(raw_input(u'数据类型【1、商品 2、股指】：'))
-    night = 0
+    type = int(raw_input(u'合约类型【1、商品 2、股指】：'))
+    if type != 1 and type != 2:
+        print '输入有误'
+        exit(0)
+
     if type == 1:
-        night = int(raw_input(u'夜盘时间【1、（21:00 - 23:00) 2、(21:00 - 23:30)】：'))
+        night = int(raw_input(u'夜盘时间类型【0、无 1、夜间 2、凌晨】：'))
+        if night != 0 and night != 1 and night != 2:
+            print '输入有误'
+            exit(0)
     engine = csvsLocalEngine(type, night)
     engine.startWork()
--- quant/test.py
+++ quant/test.py
@@ -3,7 +3,24 @@
 import pandas as pd
 import tushare as ts
 from datetime import time, timedelta
+import multiprocessing
+
+
+def testMulti(args):
+    print args
+    return 1
 
 if __name__ == '__main__':
-    a = time(6, 6, 6,500)
-    print a
+    # 多进程优化，启动一个对应CPU核心数量的进程池
+    pool = multiprocessing.Pool(multiprocessing.cpu_count())
+    l = []
+
+    settingList = {'openLen': 2}
+    for setting in settingList:
+        l.append(pool.apply_async(testMulti, ('a')))
+    pool.close()
+    pool.join()
+
+    # 显示结果
+    resultList = [res.get() for res in l]
+    print resultList
