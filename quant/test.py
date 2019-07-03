845730b91cff6f15ce3e51c303cecbe1be1562a5
loe <lo-e@outlook.com>
2019-07-03 22:43:59 +0800

    update

--- quant/test.py
+++ quant/test.py
@@ -1,42 +1,7 @@
 # -- coding: utf-8 --
 
-import pymongo
-from vnpy.trader.app.ctaStrategy.ctaBase import TICK_DB_NAME
-from vnpy.trader.app.ctaStrategy.ctaBase import DAILY_DB_NAME
-from datetime import datetime
-from vnpy.trader.app.ctaStrategy.stgEarningManager import stgEarningManager
-from collections import OrderedDict
-import matplotlib.pyplot as plt
-import pandas as pd
-import rqdatac as rq
-from rqdatac import *
-import re
-from csv import DictReader
-import pandas as pd
-
-class SliceIdGenerator:
-    """slice id生成器"""
-    def __init__(self):
-        self.__ch = 'aaaaaaaaa'
-
-    def getNextSliceId(self):
-        ch = self.__ch
-        j = len(ch) - 1
-        while j >= 0:
-            cj = ch[j]
-            if cj != 'z':
-                ch = ch[:j] + chr(ord(cj) + 1) + ch[j+1:]
-                break
-            else:
-                ch = ch[:j] + 'a' + ch[j+1:]
-                j = j -1
-        self.__ch = ch
-        return self.__ch
-
-
 if __name__ == '__main__':
-    a = 'rb1905'
-    startSymbol = re.sub("\d", "", a)
-    print startSymbol
+    print(__file__)
+
 
 
