00d17545612b1216b9a7c274d4b404c9da24d721
loe <lo-e@outlook.com>
2020-04-17 10:28:05 +0800

    update

--- quant/test.py
+++ quant/test.py
@@ -1,10 +1,20 @@
 # -- coding: utf-8 --
 
-from pathlib import Path
+from datetime import datetime
+from vnpy.trader.object import BarData
+from vnpy.trader.object import TradeData
+from vnpy.trader.constant import Exchange
 
 if __name__ == '__main__':
-    path1 = Path(__file__).parent.parent
-    print(path1)
+    #a = BarData(gateway_name='', symbol='', exchange=None, datetime=None, endDatetime=None)
+    a = TradeData(gateway_name='abc', symbol='abc', exchange=Exchange.RQ, orderid='abc', tradeid='abc')
+    b = a.__dict__
+    c = f'{b}'
+    print(type(b))
+    print(type(c))
+    print(c)
+
+
 
 
 
