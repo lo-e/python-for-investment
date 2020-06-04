16355d855333d4fa41e6083b3e629a9f771f1350
loe <lo-e@outlook.com>
2020-06-04 13:48:13 +0800

    update test of removing eos

--- quant/test.py
+++ quant/test.py
@@ -9,7 +9,7 @@ def test_tick():
     client = MongoClient('localhost', 27017, serverSelectionTimeoutMS=600)  # Mongo连接
     client.server_info()
     dbTick = client[TICK_DB_NAME]
-    symbol_list = ['BTCUSD', 'ETHUSD', 'EOSUSD']
+    symbol_list = ['BTCUSD', 'ETHUSD']
     for symbol in symbol_list:
         cl = dbTick[symbol]
         cl.create_index('datetime')  # 添加索引
@@ -24,29 +24,33 @@ def test_tick():
             tick.__dict__ = dic
             print(f'vt_symbol:{tick.vt_symbol}\t{tick.datetime}\t{tick.symbol}\t{tick.last_price}')
             count += 1
-            if count >= 1:
+            if count >= 6:
+                print('\n')
                 break
 
 def test_minute():
     client = MongoClient('localhost', 27017)
     db = client[MINUTE_DB_NAME]
-    collection = db['BTCUSD.BYBIT']
-    start = datetime.strptime('2020-3-24 00:00:00', '%Y-%m-%d %H:%M:%S')
-    end = datetime.strptime('2020-3-25 00:00:00', '%Y-%m-%d %H:%M:%S')
-    flt = {'datetime': {'$gte': start,
-                        '$lte': end}}
-    cursor = collection.find().sort('datetime', DESCENDING)
-    count = 0
-    for dic in cursor:
-        print(dic['vt_symbol'], dic['datetime'], dic['open_price'])
-        count += 1
-        if count >= 6:
-            break
+    symbol_list = ['BTCUSD.BYBIT', 'ETHUSD.BYBIT']
+    for symbol in symbol_list:
+        collection = db[symbol]
+        start = datetime.strptime('2020-3-24 00:00:00', '%Y-%m-%d %H:%M:%S')
+        end = datetime.strptime('2020-3-25 00:00:00', '%Y-%m-%d %H:%M:%S')
+        flt = {'datetime': {'$gte': start,
+                            '$lte': end}}
+        cursor = collection.find().sort('datetime', DESCENDING)
+        count = 0
+        for dic in cursor:
+            print(dic['vt_symbol'], dic['datetime'], dic['open_price'])
+            count += 1
+            if count >= 6:
+                print('\n')
+                break
 
 def test_daily():
     client = MongoClient('localhost', 27017)
     db = client[DAILY_DB_NAME]
-    symbol_list = ['BTCUSD.BYBIT', 'ETHUSD.BYBIT', 'EOSUSD.BYBIT']
+    symbol_list = ['BTCUSD.BYBIT', 'ETHUSD.BYBIT']
     # symbol_list = ['BTC.USDT.OKEX', 'ETH.USDT.OKEX', 'EOS.USDT.OKEX']
     # symbol_list = ['OKEX/BTC.USDT', 'OKEX/ETH.USDT', 'OKEX/EOS.USDT']
     # symbol_list = ['Poloniex/BTC.USDT', 'Poloniex/ETH.USDT', 'Poloniex/EOS.USDT']
@@ -67,7 +71,7 @@ def test_daily():
             close_price = dic['close_price']
             print(f'{symbol}\t{the_datetime}\t{open_price}\t{high_price}\t{low_price}\t{close_price}')
             count += 1
-            if count == 6:
+            if count >= 6:
                 print('\n')
                 break
 
